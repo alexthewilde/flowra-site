bfd30040980210d91b5e185cbff53e8bc9797327
Alex Wilde <alexosoft@gmail.com>
2018-01-07 15:02:53 +0100

    Self-host fonts.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
+// Fonts
+require('typeface-questrial');
+require('typeface-quicksand');
+
 import './../css/styles.scss';
 
 const TemplateWrapper = ({ children }) => (
