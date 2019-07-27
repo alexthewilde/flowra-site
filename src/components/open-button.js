95a61332c7510f49c03c8e44fb9e51f9f2af0849
Alex Wilde <alexosoft@gmail.com>
2019-07-27 17:15:49 +0200

    Fix.

--- src/components/open-button.js
+++ src/components/open-button.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import URLSearchParams from 'url';
 
 class OpenButton extends React.Component {
   constructor() {
     super();
 
-    const urlParams = new URLSearchParams(window.location.search);
-    const returnPath = urlParams.get('r');
+    let urlParams, returnPath = null;
+    if (typeof window != 'undefined') {
+      let urlParams = new URLSearchParams(window.location.search);
+      let returnPath = urlParams.get('r');
+    }
     this.verb = returnPath ? 'back' : 'open';
     this.object = returnPath ? ' to flowra' : 'flowra';
     this.href = 'https://www.flowra.com' + (returnPath ? returnPath : '/');
