d349384bd7ea9b29578752082161da07e9aede03
Alex Wilde <alexosoft@gmail.com>
2019-08-05 12:44:08 +0200

    Install Google Tag Manager.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,29 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-PS8BPVH",
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: true,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: {
+          type: `object`,
+          value: { pageCategory: `home` },
+        }
+
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+      },
+    },
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-favicon`,
